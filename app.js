a44038f087da084f1a1aa99d5b21164b8fedf235
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-07 10:39:42 +0530

    basic operators

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 // alert("This is an alert!") // runs on browser
 // prompt("What is your name?") // runs on browser
 console.log("Hello Sandeep"); // runs on console
+
+if(true){
+    var num1 = 10
+    console.log("inside if var " + num1);
+}
+console.log("outside if var " + num1);
