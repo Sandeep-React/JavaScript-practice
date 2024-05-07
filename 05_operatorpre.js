dab81cc7d047ba37237d58221064985c04a6528d
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-07 11:19:59 +0530

    Operator precedence

---
+++ 05_operatorpre.js
@@ -0,0 +1,16 @@
+//Operator precedence | BODMAS rule
+
+let a = 45 + 22;
+console.log(a)
+
+let b = 23 - 56/2
+console.log(b)
+
+// b = 23 -28
+// b = -5
+
+// BODMAS rule
+// (), /, *, +  and -
+
+let c = (42 -2) / 2
+console.log(c)
--- app.js
+++ app.js
@@ -4,6 +4,14 @@ console.log("Hello Sandeep"); // runs on console
 
 if(true){
     var num1 = 10
-    console.log("inside if var " + num1);
+    // console.log("inside if var " + num1);
 }
-console.log("outside if var " + num1);
+// console.log("outside if var " + num1);
+
+// let a = 12
+// console.log(a++);
+// console.log(a);
+
+let a = 12
+let b = "12"
+console.log(a+b);
