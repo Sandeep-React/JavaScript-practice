69a0bf87caaafd789be8515e3dc8f20e20d693bb
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-07 08:51:24 +0530

    variables and datatypes

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+a = "javascript";
+b = 12
+
+/* Variables name rules 
+--------------------------
+Names can contains letters, digits, underscore, and dollar signs.
+Names must begin with a letter.
+Names can also begin with $ and _ (but we rarely use it)
+Names are case sensitive (y and Y are different variable names)
+Reserved words like key words cannot be used as names.
+JavaScript identifiers are case sensitive.
+
+
+*/
--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-// alert("This is an alert!")
+// alert("This is an alert!") // runs on browser
+// prompt("What is your name?") // runs on browser
+console.log("Hello Sandeep"); // runs on console
