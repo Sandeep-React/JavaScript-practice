993ed47dce0cc08025a1fd31f51e925e4f1cfa05
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-07 11:45:34 +0530

    ...

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ if(true){
 // console.log(a++);
 // console.log(a);
 
-let a = 12
-let b = "12"
+// let a = 12
+// let b = "12"
 // console.log(a+b);
 
+console.log(null === null) //true
+console.log(undefined === undefined)//true
+
+var a = 5;
+var b = 10;
+console.log(--a); //4
+console.log(b--); // 10
+console.log(b); // 9
