9b66e2a811c0634ab57724c44e22d90e4f5ecf2d
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-08 17:36:13 +0530

    ..

--- 02JavaScript_Fundamentals_2/app.js
+++ 02JavaScript_Fundamentals_2/app.js
@@ -1,1 +1,12 @@
 console.log("JavaScript fundamentals -- II")
+
+// var message;
+//    console.log("Message: " + message)
+//    console.log("wishes: " + hello);
+
+
+var y =  10 % '5';
+console.log(y)
+   console.log(typeof y);
+   var z =  10 + '5';
+   console.log(typeof z);
