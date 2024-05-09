52e39ead2345a9383203b14d8b386587685ace31
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-09 12:33:18 +0530

    switch case

---
+++ 02JavaScript_Fundamentals_2/06switch.js
@@ -0,0 +1,37 @@
+// switch case
+
+const key = 4
+
+switch(key){
+    case 1 :
+         console.log("Door 1 opened")
+         break;
+    case 2 : 
+         console.log("Door 2 opened")
+         break;
+    case 3 : 
+         console.log("Door 3 opened")
+         break;
+    case 4 : 
+         console.log("Door 4 opened")
+         break;
+    case 5 : 
+         console.log("Door 5 opened")
+         break;
+    case 6 : 
+         console.log("Door 6 opened")
+         break;
+    case 7 : 
+         console.log("Door 7 opened")
+         break;
+    case 8 : 
+         console.log("Door 8 opened")
+         break;
+    case 9 :
+         console.log("Door 9 opened")
+         break;
+    case 10 : 
+         console.log("Door 10 opened")
+         break;
+    default: console.log("No door matched")
+}
