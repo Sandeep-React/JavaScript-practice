70cc4ffbdc502c627713f7f6fb27833ac302bfcf
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-09 12:02:59 +0530

    boolean logic

---
+++ 02JavaScript_Fundamentals_2/04booleanlogic.js
@@ -0,0 +1,13 @@
+// Boolean Logic
+
+// true and false
+
+if (true) {
+    console.log("true")
+} else {
+    console.log(false)
+}
+
+
+
+(false)?console.log("true statement"):console.log("false statement")
