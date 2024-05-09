645391a310d17e2dfc55fcb9a8817926e322c46c
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-09 13:22:16 +0530

    ternary operator

---
+++ 02JavaScript_Fundamentals_2/08ternary.js
@@ -0,0 +1,19 @@
+// Ternary operator is the another way of using if-else statement
+
+// ? :
+
+if (false) {
+    console.log("true statement")
+} else {
+    
+    console.log("false statement")
+}
+
+// using ternary operator
+
+false ? console.log("true statement") : console.log("false statement");
+
+// find if a number is even or odd
+
+ let num = 11;
+(num % 2 == 0) ? console.log("Even number") : console.log("Odd Number")
