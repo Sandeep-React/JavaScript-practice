bbcbaae3981cdcbd31e59f7c98465663cd6eeb37
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-09 12:51:51 +0530

    statement and expression

---
+++ 02JavaScript_Fundamentals_2/07statement-exp.js
@@ -0,0 +1,22 @@
+// Expression
+/* 
+    A piece of code that produces a 
+    examples
+    1+1
+    45+45
+*/
+
+
+// Statement
+
+/* 
+    A pice of code that does not produce a value
+    
+    example
+
+    if(true){
+
+    }
+
+    let a = 45 + 35 is a statement.
+*/
