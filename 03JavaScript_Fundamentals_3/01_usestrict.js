7a50e0bfaeddf88baf7f2afadb58f6094beb895a
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-09 14:44:40 +0530

    functions

---
+++ 03JavaScript_Fundamentals_3/01_usestrict.js
@@ -0,0 +1,12 @@
+/* 
+ES6 is a dynamically typed programming language
+---> no need the declare the datatype of variable
+
+Loosely packed programming Language
+---> We won't get errors but we get warnings a lot
+*/
+
+"used strict" // introduced in ES5
+
+a = 12;
+console.log(a)
---
+++ 03JavaScript_Fundamentals_3/02_functions.js
@@ -0,0 +1,21 @@
+//Function --> functions are the block of code which get executed when we call/invoke it.
+
+function a(){
+    // console.log("I have been called");
+}
+
+a() // 
+
+function adds(a,b){ // parameters
+    // console.log(a+b)
+}
+
+adds(12,14) // arguments
+adds(32,15)
+
+
+function c(){
+    return "Hey i am here 😊"
+}
+ var res = c()
+ console.log(res)
