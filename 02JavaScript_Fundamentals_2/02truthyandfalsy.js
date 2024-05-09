5034d84cfed5d220bfdecfc8f36a0e5541db0ef7
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-09 11:22:51 +0530

    truthy and falsy values

---
+++ 02JavaScript_Fundamentals_2/02truthyandfalsy.js
@@ -0,0 +1,15 @@
+// In javaScript there are value which are treated as true and false
+
+// True values are called Truthy Values
+
+// False values are called Falsy Values
+
+if (1) {
+    console.log("Condition is true")
+} else {
+    
+    console.log("Condition is false")
+}
+
+// falsy -> false, " ", ' ', 0, -0, 0n, NaN, null, undefined
+// truthy -> anything other than the falsy values
