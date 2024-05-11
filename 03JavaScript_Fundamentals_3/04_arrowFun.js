8eb17f1e7bc8612d52b70c6a22d11813804e2fdb
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-11 11:02:16 +0530

    Arrow function

---
+++ 03JavaScript_Fundamentals_3/04_arrowFun.js
@@ -0,0 +1,18 @@
+function myFunctionOne(){
+    console.log("Function one");
+}
+myFunctionOne()
+
+const myFunctionTwo = function(){
+    console.log("Function two")
+}
+myFunctionTwo()
+
+
+// Arrow function | fat arrow function 
+
+ const myArrowFunction = () => {
+    console.log("I'm a Arrow function")
+ }
+
+ myArrowFunction()
