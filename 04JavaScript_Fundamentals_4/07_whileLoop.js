9ec853eaac59bf27624268acc846879990887087
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-13 20:23:23 +0530

    while loop

---
+++ 04JavaScript_Fundamentals_4/07_whileLoop.js
@@ -0,0 +1,25 @@
+// while loop
+
+let i = 0;
+while( i < 5){
+    i++
+    // console.log(i)
+}
+
+// do - while
+
+let j = 0
+do{
+    j++
+    // console.log(j)
+}while(j<5)
+
+
+    var a = 0;
+    var b = 0;
+    while(a<3) {
+         a++; //1 2
+         b += a; // b = b+a 0+1 , 1+2 ,
+         console.log("b = "+b)//1 , 3 ,6
+         console.log("a = "+a) //1, 2, 3
+    }
