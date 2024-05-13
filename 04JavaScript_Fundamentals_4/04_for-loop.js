2c84b72a9db58b86060bea8b74281235045fe925
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-13 15:27:37 +0530

    for loop

---
+++ 04JavaScript_Fundamentals_4/04_for-loop.js
@@ -0,0 +1,23 @@
+// console.log("javaScript :1")
+// console.log("javaScript :2")
+// console.log("javaScript :3")
+// console.log("javaScript :4")
+// console.log("javaScript :5")
+
+// We can use the for loop to do the repetitive tasks
+
+/* Syntax 
+    for (start; condition; inc/dec)
+*/
+    for(let i = 1 ; i <= 5; i++){
+        console.log(`JavaScript ${i}`)
+    }
+
+    // 
+
+    for(let i = 0 ; i <= 10; i=i+2){
+        console.log(i)
+    }
+
+
+
