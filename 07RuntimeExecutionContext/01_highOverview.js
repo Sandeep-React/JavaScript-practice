c9481e1d500efd7a716d566ba543231c0ef7bab2
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-17 11:16:30 +0530

    Js engine and runtime environment

--- 07RuntimeExecutionContext/01_highOverview.js
+++ 07RuntimeExecutionContext/01_highOverview.js
@@ -4,3 +4,14 @@
    JS: Dynamically typed | loosely packed | JIT | Object based programming
 */
 
+//JavaScript Engine
+/* 
+    javascript code  ----> parse it -----> Abstract syntax tree ---->interpreter --------> Bytecode
+                                                                        |                   ^
+                                                                        |                   |
+                                                                Optimizing Compiler ---->Machine Code    
+*/
+
+// Chrome use V8 Engine
+// Compiler ---> turbo fan
+// interpreter --> Ignitor
