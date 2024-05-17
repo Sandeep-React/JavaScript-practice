ccd4891095d089e0b66fe4b823387852539f9da7
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-17 10:44:59 +0530

    Overview of javaScript | JIT

---
+++ 07RuntimeExecutionContext/01_highOverview.js
@@ -0,0 +1,6 @@
+/* Compiler: Translate in one go
+   Interpreter: Translate line by line
+   JIT (Just in time): When we combine both compiler and interpreter
+   JS: Dynamically typed | loosely packed | JIT | Object based programming
+*/
+
