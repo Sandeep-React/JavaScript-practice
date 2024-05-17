8a4ce001aa3716eec5fa5e349fb020feb258b32e
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-05-17 12:01:24 +0530

    Execution context

---
+++ 07RuntimeExecutionContext/02_ExecutionContex.js
@@ -0,0 +1,15 @@
+//Execution Context
+// 1.  Global Execution context -------- GEC
+// 2.  Functional Execution context----- FEC
+
+// Global execution context have two part
+// 1. Environment variable
+// 2. Thread of execution
+
+let a = 12
+console.log(a)
+
+
+// console.log(b)
+let b =13
+console.log(b)
