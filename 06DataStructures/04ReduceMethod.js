8a2be4eac1f175459f946d2b995e9baaaa8977fd
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-07-25 11:59:14 +0530

    Reduce method

---
+++ 06DataStructures/04ReduceMethod.js
@@ -0,0 +1,18 @@
+const movements = [200, -450, 400, 3000, -650, -130, 70, 1300];
+
+const balance = movements.reduce(function(acc, mov, index, array){
+    console.log((`Iteration no : ${index + 1}: Acc value is ${acc}`))
+    return acc + mov 
+
+},0);
+console.log(balance)
+
+// Implement the same using forOf 
+
+let balance2 = 0;
+for(const mov of movements){
+    balance2 += mov
+
+}
+
+console.log(balance2)
