91bb613b81cc86579632129820af09ca3e9ff5f2
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-07-28 08:20:16 +0530

    Modulo-Operator

---
+++ 07Datastructures/03Modulo-Operator.js
@@ -0,0 +1,14 @@
+console.log(5/2)
+console.log(4/2)
+console.log(5%2)
+console.log(8%3)
+console.log(7%2)
+
+console.log(6%2)
+console.log(9%2)
+
+const isEven = n => n%2 === 0
+
+console.log(isEven(8))
+console.log(isEven(213))
+console.log(isEven(578))
