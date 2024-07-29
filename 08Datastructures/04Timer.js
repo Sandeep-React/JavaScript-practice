7f79afb95ec3da37421c2bd38ab1d6d1fa57bc5b
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-07-29 13:31:14 +0530

    Timer' git commit -m Timer--

---
+++ 08Datastructures/04Timer.js
@@ -0,0 +1,44 @@
+// Timers 
+
+// Set Intervals -- repeats a given function at every given time interval. 
+// set Timeout -- executes a function, after waiting a specified number of milliseconds
+
+/* SetTimeout() */
+
+// setTimeout(() =>{
+//     console.log("Here is your Pizza")
+// },5000)
+
+// console.log("I am ordering a pizza")
+
+// setTimeout((ing1, ing2) =>{
+//     console.log(`Here is your Pizza with ${ing1} and ${ing2}` )
+// },5000, 'olives', 'mushrooms')
+
+// console.log("Here is my pizza")
+
+console.log("I am ordering a pizza")
+
+const ing = ['olives', 'mushrooms']
+setTimeout((ing1, ing2) =>{
+    console.log(`Here is your Pizza with ${ing1} and ${ing2}` )
+},5000,...ing)
+
+console.log("Here is my pizza")
+
+//We can cancel the timeout in the following way:
+
+console.log("I am ordering a pizza")
+
+const ingred = ['olives', 'mushrooms']
+const pizzaTimer = setTimeout((ing1, ing2) =>{
+    console.log(`Here is your Pizza with ${ing1} and ${ing2}` )
+},5000,...ingred)
+
+if(ingred.includes('olives')){
+    clearTimeout(pizzaTimer)
+}
+
+console.log("Here is my pizza")
+
+
