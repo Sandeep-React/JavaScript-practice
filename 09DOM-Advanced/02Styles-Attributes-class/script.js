771cc55cb2c74308f1325059ee8d635cb9e0773a
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-07-31 08:03:13 +0530

    Style Attributeand class

--- 09DOM-Advanced/02Styles-Attributes-class/script.js
+++ 09DOM-Advanced/02Styles-Attributes-class/script.js
@@ -31,11 +31,11 @@ message.innerHTML = 'We use cookies for functionality and and analytics <button 
 
 // inserting element 
 console.log(message)
-// header.prepend(message)
+header.prepend(message)
 // header.append(message)
 
-header.before(message)
-header.after(message)
+// header.before(message)
+// header.after(message)
 
 
 
@@ -44,4 +44,61 @@ document.querySelector('.btn-close-cookie').addEventListener('click', function()
     // message.parentElement.removeChild(message);
     message.remove() // This is the a new method
 })
+
+// Styles attribute and classes
+
+//----> use camel case
+
+
+message.style.backgroundColor = "#37383d"
+
+message.style.height = '80px';
+
+console.log(message.style.backgroundColor)
+console.log(message.style.width) // this will only work for the proporties which added via Javascirpt
+
+console.log(getComputedStyle(message).width)
+console.log(getComputedStyle(message).color)
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 30 + 'px';
+console.log(getComputedStyle(message).height)
+
+// 
+// document.documentElement.style.setProperty('--color-primary','red')
+
+//Attributes 
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+
+console.log(logo.className)
+
+logo.alt = "A modern logo";
+
+//To access a custom designed attribute
+console.log(logo.designer) // Undefined
+console.log(logo.getAttribute('designer'))
+console.log(logo.getAttribute('designer'))
+
+//To access a data attribute
+console.log(logo.dataset.versionNumber)
+
+console.log(logo.src) // we get absolute URL
+console.log(logo.getAttribute('src')) // we get the relative URL
+
+
+
+// classes 
+logo.classList.add('c', 'j')
+logo.classList.remove('c','j')
+logo.classList.toggle('c')
+logo.classList.contains('c');
+
+
+
+// Don't do this 
+
+//Update the classes
+logo.classList ="dark-mode"
+
+
 
