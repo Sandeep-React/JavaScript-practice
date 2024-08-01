7774596978219088cc71b753d628b44ca961c2ee
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-08-01 10:53:29 +0530

    Event Delegation: Navigation Implementation

--- 09DOM-Advanced/AdvancedDom-Starter/script.js
+++ 09DOM-Advanced/AdvancedDom-Starter/script.js
@@ -37,3 +37,64 @@ btnScrollTo.addEventListener('click',function(e){
 
     section1.scrollIntoView({behavior:'smooth'})
 })
+
+// Page Navigation
+
+// console.log(document.querySelectorAll('.nav__link'))
+// document.querySelectorAll('.nav__link').forEach(function(el){
+//     el.addEventListener('click', function(e){
+//         e.preventDefault()
+//         // console.log(this.getAttribute('href'))
+//         const id  = this.getAttribute('href')
+//         document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+//     })
+// })
+
+// More efficient code for Page navigation
+
+// Using Event Bubbling
+
+// 1 Add event listener to common parent el
+// 2 Determine what el originated the event
+
+document.querySelector('.nav__links').addEventListener('click',function(e){
+    // console.log("click")
+    e.preventDefault()
+// console.log(e.target)
+
+    if(e.target.classList.contains('nav__link')){
+        const id = e.target.getAttribute('href')
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+    }
+})
+
+
+
+
+// Practical Application onn Event Propagation
+
+// const randomInt = (min,max) => 
+//     Math.floor(Math.random() * (max-min + 1) + min)
+// console.log(randomInt(0,255))
+
+// const randomColor = () =>
+//     `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`
+
+//     console.log(randomColor())
+
+
+// document.querySelector('.nav__link').addEventListener('click', function(e){
+//     console.log("LINK",e.target,e.currentTarget)
+//     this.style.backgroundColor =randomColor()
+
+//     // Stop propagation 
+//     e.stopPropagation()
+// })
+// document.querySelector('.nav__links').addEventListener('click', function(e){
+//     console.log("Container",e.target,e.currentTarget)
+//     this.style.backgroundColor =randomColor()
+// })
+// document.querySelector('.nav').addEventListener('click', function(e){
+//     console.log("Nav",e.target,e.currentTarget)
+//     this.style.backgroundColor =randomColor()
+// })
