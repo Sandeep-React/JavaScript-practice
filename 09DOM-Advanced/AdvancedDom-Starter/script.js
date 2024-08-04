738092a3335dd2274c5dbc08a125f6606a16dbc1
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-08-04 17:42:55 +0530

    Loading Lazy images

--- 09DOM-Advanced/AdvancedDom-Starter/script.js
+++ 09DOM-Advanced/AdvancedDom-Starter/script.js
@@ -151,6 +151,33 @@ allSections.forEach(function(section){
 })
 
 
+// lazy loading Images for Improved performance
+
+const imgTargets = document.querySelectorAll('img[data-src]')
+
+const loadImg = function(entries,Observer){
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    entry.target.src = entry.target.dataset.src;
+    entry.target.addEventListener('load', function(){
+        entry.target.classList.remove('lazy-img')
+    })
+    observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg,{
+    root:null,
+    threshold:0
+})
+
+imgTargets.forEach(img => imgObserver.observe(img))
+
+
+
+
+
 
 
 
