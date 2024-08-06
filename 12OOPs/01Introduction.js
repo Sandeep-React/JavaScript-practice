226c563eed54863048c9b2854975d9faff76f0b2
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-08-06 14:44:05 +0530

    OOPs in JavaScript

---
+++ 12OOPs/01Introduction.js
@@ -0,0 +1,27 @@
+// Object Oriented Programming Language
+/* OOPs in Java Script
+--------------------------
+Prototype -- like a class contain some method
+From this prototype we create an Object and can access the methods
+
+Prototypal inheritance
+-----------------------
+The prototype contains methods tht are accessible to all objects linked to that prototype;
+Behavior is delegated to the linked prototype object.
+
+3 ways to implement the prototypal inheritance
+----------------------------------------------
+1) Constructor function
+    Technique to create objects from a function
+    This is how built in objects like Arrays, maps or sets are actually implemented.
+
+2) ES6 classes
+    Modern alternative to connect function syntax
+    "Syntactic sugar" : behind the screen, ES6 classes works like constructor function.
+    ES6 classes do  not behave like classes in " CLASSICAL OOP"
+
+3) Object.create()
+    The easiest and the most straightforward way of linking an object to prototype
+
+*/
+
