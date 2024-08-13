6493c7791127399a8534721a5fd7cabb8da1b7b8
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-08-13 16:29:31 +0530

    Understanding Asynchronous JavaScript, AJAX, and APIs

---
+++ 14Asynchronous-JavaScript/2236606-Starter_Files/01Introduction.js
@@ -0,0 +1,31 @@
+// 1.Understanding Asynchronous JavaScript, AJAX, and APIs 
+
+/* synchronous code
+--------------------
+--Most of the code are synchronous code
+--synchronous code is executed line by line
+--Each line of code waits for the previous line to finish
+--Long lasting operation block code execution 
+*/
+
+/* Asynchronous code
+--------------------
+--Asynchronous code is executed after a task that rus in the background finishes.
+--Asynchronous cde non-blocking.
+--Execution doesn't wait for an asynchronous task to finish it's work.
+--Callback functions alone do not make code asynchronous.
+--Examples- Image Loading, Geolocation API, AJAX call.
+*/
+
+/* AJAX-- Asynchronous JavaScript And XML: Allows us to communicate with remote web servers in an asynchronous way.
+          With aJAX call, we can request data from web servers dynamically.
+          
+*/
+
+/* APIs-- Application programming Interface, Piece of software that can be used by another piece of software,
+in order to talk application to talk to each other.
+
+Most popular Uses API data formate is JSON  
+
+Examples; DOM API, Geolocation API, Own class API, Online apis(APIs)
+*/
