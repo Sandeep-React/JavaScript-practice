6218efc8abcd0e160dbc94cf3d06ef7ff8fd3c8a
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-08-13 08:33:22 +0530

    Encapsulation: Protected Properties and Methods

---
+++ 13ES6-OOPs/06Encapsulation--1.js
@@ -0,0 +1,42 @@
+// Encapsulation: Protected Properties and Methods
+// Another Example of Class Implementation
+
+class Account {
+    constructor(owner,currency, pin,){
+        this.owner = owner;
+        this.currency = currency;
+        this._pin = pin;
+        this._movements = [];
+        this.locale = navigator.language
+    }
+
+    //Public Interfaces
+    deposit(val){
+        this._movements.push(val)
+    }
+    withdraw(val){
+        this.deposit(-val)
+    }
+
+    _approveLoan(){
+        return true;
+    }
+
+    requestLoan(val){
+        if(this.approveLoan){
+            this.deposit(val)
+        }
+    }
+}
+
+const acc1 = new Account('Sandeep', 'INR', 1111);
+acc1.deposit(450)
+acc1.withdraw(200)
+acc1.requestLoan(500)
+acc1._approveLoan();
+
+acc1._movements.push(500);
+
+console.log(acc1._pin)
+
+console.log(acc1)  // Use browser to run
