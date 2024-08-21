31b9c2ceaa2bc1448e8a63b6b46e7c65a8b061a3
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-08-21 12:36:53 +0530

    Handling Errors with try...catch Block

---
+++ 15Asynchronous-JavaScript-2/06Try--Catch-block.js
@@ -0,0 +1,30 @@
+// Handling Errors with try...catch Block
+
+
+
+const whereAmI = async function () {
+
+    try{
+
+        
+        const res = await fetch(`https://api.github.com/users/Sandeep-React`)
+        const data = await res .json();
+        console.log(data)
+    }catch{
+        console.err(err.message);
+        console.log('Where am i error', error)
+        
+    }
+    
+}
+
+whereAmI()
+console.log("first log")
+
+// try{
+//     let y = 1
+//     const x = 4
+//     x=3;
+// } catch (err){
+//     console.log(err.message)
+// }
