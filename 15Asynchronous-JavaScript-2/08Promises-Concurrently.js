7d1cafa7798db9e68895c0d0ac3686b15d960249
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-08-21 15:26:25 +0530

    Running Promises Concurrently

---
+++ 15Asynchronous-JavaScript-2/08Promises-Concurrently.js
@@ -0,0 +1,32 @@
+// Running Promises Concurrently
+
+const getJSON = function(url, errorMsg = "Something went wrong") {
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+        return response.json(); // Convert response to JSON
+      });
+  };
+
+const get3Users = async function (u1, u2, u3) {
+    try {
+        // const user1 = await getJSON(`https://api.github.com/users/${u1}`)
+        // const user2 = await getJSON(`https://api.github.com/users/${u2}`)
+        // const user3 = await getJSON(`https://api.github.com/users/${u3}`)
+
+        // console.log(user1.location, user2.location, user3. location)
+
+        const data = await Promise.all([
+            getJSON(`https://api.github.com/users/${u1}`),
+            getJSON(`https://api.github.com/users/${u2}`),
+            getJSON(`https://api.github.com/users/${u3}`)
+        ])
+        data.map((d) => {
+            console.log(d.location)
+        })
+    } catch (error) {
+        console.error(error, "😑😐")
+    }    
+}
+
+get3Users("mojombo", "wycats", "pjhyett")
